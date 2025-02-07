5a512a28c78683f507f689092b157761601edc5c
Morten Rode Enghausen <morten@enghausen.dk>
2025-02-07 09:50:10 +0100

    Wrong folder for exercise_0 solution and solved exercise_1

--- solutions/exercise_1.cpp
+++ solutions/exercise_1.cpp
@@ -1,8 +1,39 @@
+/* Implement a program that prompts the user to input an integer, and subsequently determines the numbers sign and parity (even or odd). The program should present the following output:
+The number is positive and even.
+The number is positive and odd.
+The number is negative and even.
+The number is negative and odd.
+The number is zero.
+Depending on the number of course
+Hint Nested if statement can be used here. To test an integer for parity, use the modulus operator mod (%), which gives the remainder in division. That is 7 mod 3 = 1 because the remainder is 1 , 23 mod 3 = 2 because the remainder 2 . In this way x mod 2 == 0 if x is even, and x mod 2 ≠ 0 if x is odd. */
+
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
   cout << "exercise_1.cpp" << endl;
+
+  int number;
+  cout << "Enter an integer: ";
+  cin >> number;
+
+  if (number > 0) {
+    if (number % 2 == 0) {
+      cout << "The number is positive and even." << endl;
+    } else {
+      cout << "The number is positive and odd." << endl;
+    }
+  } else if (number < 0) {
+    if (number % 2 == 0) {
+      cout << "The number is negative and even." << endl;
+    } else {
+      cout << "The number is negative and odd." << endl;
+    }
+  } else {
+    cout << "The number is zero." << endl;
+  }
+
   return 0;
 }
--- src/exercise_0.cpp
+++ src/exercise_0.cpp
@@ -1,31 +1,8 @@
-// Implement a program taking two integers as input
-// And that writes to output: if The first is greater than the second (if that is the case!) else if The second is greater than the first (if that is the case!) else The numbers are equal
-// The program should additionally write the sum and the product of the numbers to output
-
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
+int main() {
   cout << "exercise_0.cpp" << endl;
-
-  int a, b;
-  cout << "Enter first integer: " << endl;
-  cin >> a;
-  cout << "Enter second integer: " << endl;
-  cin >> b;
-  if (a > b)
-  {
-    cout << "The first is greater than the second" << endl;
-  }
-  else if (b > a)
-  {
-    cout << "The second is greater than the first" << endl;
-  }
-  else
-  {
-    cout << "The numbers are equal" << endl;
-  }
   return 0;
 }
